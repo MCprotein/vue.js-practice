ed7d04412e252039cdfd536f92b106224b8543ae
chss3339 <chss3339@gmail.com>
2022-09-20 00:51:37 +0900

    fix: 라우터 옵션에 webpackPrefetch  추가

--- project/src/router/index.js
+++ project/src/router/index.js
@@ -14,7 +14,9 @@ const routes = [
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () =>
-      import(/* webpackChunkName: "about2" */ '../views/AboutView.vue')
+      import(
+        /* webpackChunkName: "about", webpackPrefetch:true */ '../views/AboutView.vue'
+      )
   }
 ]
 

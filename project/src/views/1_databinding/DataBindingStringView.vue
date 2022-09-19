a92671bbff6341fd09244b2e416b8c85bed25ec8
chss3339 <chss3339@gmail.com>
2022-09-20 01:07:18 +0900

    feat: 데이터바인딩 연습 뷰 틀 작성

---
+++ project/src/views/1_databinding/DataBindingStringView.vue
@@ -0,0 +1,24 @@
+<template>
+  <!-- html 태그가 들어가는 부분 -->
+  <div>
+    <h1></h1>
+    <p></p>
+  </div>
+</template>
+<script>
+// 자바스크립트 코드
+export default {
+  // function data()인데 vue에서는 function 생략
+  data() {
+    return {
+      userName: 'John Doe'
+    }
+  }
+}
+</script>
+
+<style scoped>
+/* 이 뷰 파일 안에서만 적용된다.
+    다른 파일에서 같은 이름을 가진 요소가 있어도 거기에는 적용안됨.
+*/
+</style>

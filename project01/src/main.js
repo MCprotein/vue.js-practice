1e6890b469c6291e5e9705a41f7ea39561c0d30a
chss3339 <chss3339@gmail.com>
2022-09-19 22:30:33 +0900

    chore: vue create project

--- project01/src/main.js
+++ project01/src/main.js
@@ -1,4 +1,5 @@
-import { createApp } from 'vue'
-import App from './App.vue'
+import { createApp } from 'vue';
+import App from './App.vue';
 
-createApp(App).mount('#app')
+// App을 가져와서 index.html의 id app에 마운트를 시킨다.
+createApp(App).mount('#app');

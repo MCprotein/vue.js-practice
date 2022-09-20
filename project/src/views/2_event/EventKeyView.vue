86bf70b675fe42f73ffc383ed3ff6559edde202e
chss3339 <chss3339@gmail.com>
2022-09-20 22:37:21 +0900

    feat: key 입력 이벤트

---
+++ project/src/views/2_event/EventKeyView.vue
@@ -0,0 +1,58 @@
+<template>
+  <div>
+    <!-- <input
+      type="search"
+      name=""
+      id=""
+      @keyup="checkEnter($event)"
+      v-model="searchText"
+    /> -->
+    <input
+      type="search"
+      name=""
+      id=""
+      @keyup.enter="doSearch"
+      v-model="searchText"
+    />
+    <button @click="doSearch">조회</button>
+    <button type="submit" @click.prevent="doSearch">조회</button>
+  </div>
+</template>
+<script>
+// .enter
+// .tab
+// .delete
+// .esc
+// .space
+// .up
+// .down
+// .left
+// .right
+// .ctrl
+// .ctrl.enter
+// .stop - event.stopPropagation()
+// .prevent - event.preventDefault()
+export default {
+  components: {},
+  data() {
+    return {
+      searchText: ''
+    }
+  },
+  setup() {},
+  created() {},
+  mounted() {},
+  unmounted() {},
+  methods: {
+    doSearch() {
+      console.log(this.searchText)
+    },
+    checkEnter(event) {
+      // 엔터키 눌렀나 확인
+      if (event.keyCode === 13) {
+        this.doSearch()
+      }
+    }
+  }
+}
+</script>
